4c4884644db25b0313b54cbab51f4a5d98c1703e
Yosef Danan <yosefdanan1234@gmail.com>
2021-10-28 23:29:12 +0300

    Extract Validator to be reused

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -15,51 +15,36 @@ import com.google.firebase.auth.FirebaseUser;
 
 import com.mobsandgeeks.saripaar.ValidationError;
 import com.mobsandgeeks.saripaar.Validator;
-import com.mobsandgeeks.saripaar.annotation.Checked;
-import com.mobsandgeeks.saripaar.annotation.ConfirmPassword;
 import com.mobsandgeeks.saripaar.annotation.Email;
-import com.mobsandgeeks.saripaar.annotation.Length;
-import com.mobsandgeeks.saripaar.annotation.Max;
-import com.mobsandgeeks.saripaar.annotation.Min;
 import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 import com.mobsandgeeks.saripaar.annotation.Password;
-import com.mobsandgeeks.saripaar.annotation.Pattern;
-import com.mobsandgeeks.saripaar.annotation.Url;
 
 import java.util.List;
 
 
-public class Login extends AppCompatActivity implements Validator.ValidationListener {
+public class Login extends FormValidator implements Validator.ValidationListener {
     static private final String TAG = "Login";
     private FirebaseAuth mAuth;
 
-    @NotEmpty()
-    @Email()
+    @NotEmpty() @Email()
     private EditText email;
-
     @Password(min=6)
     private EditText password;
 
     private Button login_button;
-    private Validator validator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         init();
-        login_button.setOnClickListener(this::onLogin);
+        login_button.setOnClickListener(this::onSubmit);
         mAuth = FirebaseAuth.getInstance();
-        validator = new Validator(this);
-        validator.setValidationListener(this);
     }
     private void init(){
         email = findViewById(R.id.email_input);
         password = findViewById(R.id.password_input);
         login_button = findViewById(R.id.login_btn);
-    }
-    private void onLogin(View v){
-        validator.validate();
     }
 
     @Override
@@ -82,19 +67,5 @@ public class Login extends AppCompatActivity implements Validator.ValidationList
                         Toast.makeText(Login.this, "Login Failed!", Toast.LENGTH_SHORT).show();
                     }
                 });
-    }
-
-    @Override
-    public void onValidationFailed(List<ValidationError> errors) {
-        for (ValidationError error : errors) {
-            View view = error.getView();
-            String message = error.getCollatedErrorMessage(this);
-            // Display error messages
-            if (view instanceof EditText) {
-                ((EditText) view).setError(message);
-            } else {
-                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-            }
-        }
     }
 }
