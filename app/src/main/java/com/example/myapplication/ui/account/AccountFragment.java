58b1bc1cb2b86a70a6197e341fbb8483f53332ab
Eyal Levi <happyeyal@gmail.com>
2021-11-13 14:39:03 +0200

    some changes after changing user auth screen in MainActivity.java

--- app/src/main/java/com/example/myapplication/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/myapplication/ui/account/AccountFragment.java
@@ -1,25 +1,35 @@
 package com.example.myapplication.ui.account;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentAccountBinding;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AccountFragment extends Fragment implements View.OnClickListener {
 
     private AccountViewModel accountViewModel;
     private FragmentAccountBinding binding;
     private Button btnMyPosts;
+    private Button logoutBtn;
+    private FirebaseAuth firebaseAuth;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         accountViewModel = new ViewModelProvider(this).get(AccountViewModel.class);
@@ -29,6 +39,14 @@ public class AccountFragment extends Fragment implements View.OnClickListener {
         btnMyPosts = binding.btnMyPosts;
         btnMyPosts.setOnClickListener(this);
         accountViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        // button for logout and initialing our button.
+        logoutBtn = root.findViewById(R.id.idBtnLogout);
+        // adding onclick listener for our logout button.
+        logoutBtn.setOnClickListener(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
         return root;
     }
 
@@ -42,8 +60,28 @@ public class AccountFragment extends Fragment implements View.OnClickListener {
     public void onClick(View v) {
 
         /*When the user clicks on the "My Posts" button in this fragment it navigates to MyPostsFragment*/
-        if(v == btnMyPosts){
+        if (v == btnMyPosts) {
             Navigation.findNavController(v).navigate(R.id.action_navigation_myAccount_to_myPostsFragment);
+        } else if (v == logoutBtn) {
+            // below line is for getting instance
+            // for AuthUi and after that calling a
+            // sign out method from Firebase.
+            AuthUI.getInstance()
+                    .signOut(getContext())
+                    // after sign out is executed we are redirecting
+                    // our user to MainActivity where our login flow is being displayed.
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            // below method is used after logout from device.
+                            Toast.makeText(getContext(), "User Signed Out", Toast.LENGTH_SHORT).show();
+
+                            // below line is to go to MainActivity via an intent.
+                            Intent i = new Intent(getContext(), MainActivity.class);
+                            startActivity(i);
+                        }
+                    });
         }
     }
 }
--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -4,15 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
