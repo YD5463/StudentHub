e0b73c432629daa10bce4dc2c8611be2eb1aae86
Eyal Levi <happyeyal@gmail.com>
2021-11-14 19:01:32 +0200

    add location property in Post.java class

--- app/src/main/java/com/example/myapplication/database/Post.java
+++ app/src/main/java/com/example/myapplication/database/Post.java
@@ -1,22 +1,31 @@
 package com.example.myapplication.database;
 
 public class Post {
-    private String title, description, uid;
+    private String title, description, uid, location;
     private int starCount, price;
 
     public Post() {
     }
 
-    public Post(String title, String description, int starCount, int price, String uid) {
+    public Post(String title, String description, int starCount, int price, String uid, String location) {
         this.title = title;
         this.description = description;
         this.starCount = starCount;
         this.price = price;
         this.uid = uid;
+        this.location = location;
     }
 
     public String getUid() {
         return uid;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
     }
 
     public void setUid(String uid) {
